2294367f0fff815e6133c152512e67a6a09e6047
wlg001 <wlg8711@qq.com>
2023-07-08 00:03:33 +0800

    aaa

--- test001.cpp
+++ test001.cpp
@@ -1,1 +1,24 @@
-//testfile
+#include<iostream>
+//main function
+int main()
+{
+    std::cout<<"Hello World!";
+    return 0;
+}
+//this is a sort function for array of integers
+void sort(int arr[],int n)
+{
+    int i,j,temp;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(arr[j+1]<arr[j])
+            {
+                temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
